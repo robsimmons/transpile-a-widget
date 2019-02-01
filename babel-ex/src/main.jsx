bec0d046e88375e21a1d771e536f0a165e9827a8
Rob Simmons <rob@calculem.us>
2019-02-01 14:57:41 -0500

    Working first babel example

--- babel-ex/src/main.jsx
+++ babel-ex/src/main.jsx
@@ -1,19 +1,29 @@
 /** @jsx h **/
 
-const {preact} = libraries;
-const {h, render, Component} = preact;
+const { preact } = libraries;
+const { h, render, Component } = preact;
 
 class MyForm extends Component {
-  constructor() {
-    super();
-  }
-    
-  render(props, state) {
-    return <center>
-      <p>I'm a widget</p>
-      <input></input>
-    </center>
-  }
-}dsaf
+    constructor() {
+        super();
+        this.state.input = "";
+        this.onInput = this.onInput.bind(this);
+    }
+
+    onInput(e) {
+        this.setState({ input: e.target.value });
+    }
+
+    render(props, state) {
+        return (
+            <center>
+                <input onInput={this.onInput} />
+                <p>
+                    I'm a widget{state.input !== "" ? ` containing the text "${<tt>state.input</tt>}"` : ""}
+                </p>
+            </center>
+        );
+    }
+}
 
 preact.render(h(MyForm), container);
